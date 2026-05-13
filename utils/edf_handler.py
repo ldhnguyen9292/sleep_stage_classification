abf57ccea6c78b4dc3d9535b1fbaf4f38161ec1c
Nguyen Le <ldhnguyen9292@gmail.com>
2026-05-13 15:13:04 +0800

    Fix mapping for sleep stage R in event ID assignment

--- utils/edf_handler.py
+++ utils/edf_handler.py
@@ -24,7 +24,7 @@ def generate_epochs_from_edf(psg_file, hyp_file, duration=30):
         'Sleep stage 2': 2,
         'Sleep stage 3': 3,
         'Sleep stage 4': 3,  # Gộp stage 3 và 4 thành stage 3 (N3)
-        'Sleep stage R': 5
+        'Sleep stage R': 4
     }
 
     events, event_id = mne.events_from_annotations(
